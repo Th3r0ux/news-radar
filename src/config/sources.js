68da7ddd8936e6cc94f9bba465fda09b499bc078
Th3r0ux <guillaumetheroux23@gmail.com>
2026-03-27 16:44:39 -0400

    Update 2e source.js

--- src/config/sources.js
+++ src/config/sources.js
@@ -2,6 +2,11 @@ export const RSS_SOURCES = [
   {
     name: 'La Presse',
     slug: 'rss-lapresse',
-    url: 'https://www.lapresse.ca/actualites/rss.xml'
+    url: 'https://www.lapresse.ca/actualites/rss'
+  },
+  {
+    name: 'Radio-Canada Info',
+    slug: 'rss-radio-canada',
+    url: 'https://ici.radio-canada.ca/rss/4159'
   }
 ]
